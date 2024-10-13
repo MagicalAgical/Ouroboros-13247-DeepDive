da89c6522e3ba9dec6c6f0072e63040c90c0ee2a
tg-32608 <125822330+tg-32608@users.noreply.github.com>
2024-10-13 15:54:00 -0500

    10/13

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueRightPractice.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.easyopencv.OpenCvPipeline;
+@Autonomous
+public class BlueRightPractice extends LinearOpMode {
+    private DcMotor leftLift = null;
+    private DcMotor rightLift = null;
+    private Servo claw = null;
+    private DcMotor Arm = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addLine("Waiting for Start");
+        telemetry.update();
+
+        claw = hardwareMap.get(Servo.class, "claw");
+        leftLift = hardwareMap.get(DcMotor.class, "leftLift");
+        rightLift = hardwareMap.get(DcMotor.class, "rightLift");
+        Arm = hardwareMap.get(DcMotor.class, "Arm");
+        int x = 0;
+        waitForStart();
+
+        if (opModeIsActive()) {
+            SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+            TrajectorySequence Left = drive.trajectorySequenceBuilder(new Pose2d())
+                    .forward(15)
+                    .lineToLinearHeading(new Pose2d(-94,1,Math.toRadians(135)))
+                    .addTemporalMarker()
+                    .build();
+        }
+    }
+}
